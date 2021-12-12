ef6d07fd02909c6d3e819e664e025cc13ea41bc1
MayankVats <vats.2610@gmail.com>
2021-12-13 00:35:54 +0530

    initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { create } from "ipfs-http-client";
+
+const client = create("http://52.14.250.56:5001/api/v0");
 
 function App() {
+  const [fileUrl, updateFileUrl] = useState(``);
+  async function onChange(e) {
+    const file = e.target.files[0];
+
+    try {
+      const added = await client.add(file);
+      const url = `https://ipfs.io/ipfs/${added.path}`;
+      updateFileUrl(url);
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IPFS Example</h1>
+      <p>{fileUrl}</p>
+      <input type="file" onChange={onChange} />
+      {fileUrl && <img src={fileUrl} width="600px" />}
     </div>
   );
 }
